7d3e9cc768ab96f17a1af10d4727b077c80d9337
hieu.nguyenvan <hieu.nguyenvan@powergateaustralia.com>
2022-09-16 17:15:26 +0700

    linear regression

---
+++ linear_regression/main.py
@@ -0,0 +1,6 @@
+def main():
+  print('hello python')
+  pass
+
+if __name__ == "__main__":
+  main()
---
+++ linear_regression/training.py
@@ -0,0 +1,3 @@
+# x = [] -> row vector
+# w = [] -> col vector
+# e sẽ là sự sai số giữa giá trị thực y và giá trị dự đoán y'
